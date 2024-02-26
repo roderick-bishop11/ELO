9860d3d9b706c430f31406052267ebecfea93130
Roderick Bishop <rbbishop11@gmail.com>
2024-02-25 18:00:54 -0600

    new voice from elevenlabs

--- assistant/assistant/__init__.py
+++ assistant/assistant/__init__.py
@@ -1,18 +1,32 @@
 import speech_recognition as sr
 import pyttsx4
 import datetime
+from openai import OpenAI
+import elevenlabs
+from elevenlabs import play
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gptKey = os.getenv("OPENAI_API_KEY")
+elevenKey = os.getenv("ELEVENLABS_API_KEY")
+client = OpenAI(api_key=gptKey)
+elevenlabs.set_api_key(api_key=elevenKey)
 
 # initialize the recognizer and engine
 recognizer = sr.Recognizer()
 ttsEngine = pyttsx4.init()
 
-#voice config
-voices = ttsEngine.getProperty('voices')
-ttsEngine.setProperty('voice', voices[140].id)
 
-def speak(audio):
-    ttsEngine.say(audio)
-    ttsEngine.runAndWait()
+def speak(text):
+
+    audio = elevenlabs.generate(
+            text=text, voice="Adam", model="eleven_monolingual_v1", api_key=elevenKey
+        )
+    play(audio)
+    # ttsEngine.say(audio)
+    # ttsEngine.runAndWait()
  
 def wishMe():
     hour = int(datetime.datetime.now().hour)
@@ -24,7 +38,6 @@ def wishMe():
   
     else:
         greeting = "Good evening, Sir"
-    print(greeting)
     speak(greeting)
 
 # the assistant will listen to you, go speech to text.
@@ -46,16 +59,34 @@ def listen():
         return "None"
      
     return query
+
 #submit text to LLM
 def textToModel(query):
-        something = ""
-        speak("I cannot answer yet, but I know what you said")
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+     messages=[
+        {"role": "system", "content": "You are a personal assistant, named ELO. ELO stands for Enhanced Logical Operative."},
+        {"role": "user", "content": query}
+        ]
+    )
+    reply = completion.choices[0].message.content
+    print(reply)
+    speak(reply)
 
+def listenForStop(query):
+    if query == "stop":
+        speak("Okay, hope I was able to help!")
+        exit()
+    elif query == "Thanks ELO that is all I needed":
+        speak("Okay, hope I was able to help!")
+        exit()
 
 def main():
     wishMe()
-    query = listen()
-    textToModel(query)
+    while True:
+        query = listen()
+        listenForStop(query)
+        textToModel(query)
 
 if __name__ == "__main__":
     main()
